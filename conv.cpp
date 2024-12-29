b91def4795eef1e1367148c010f331fe86469c9f
Richard Bell <rcbell@ucsd.edu>
2024-12-28 16:47:57 -0800

    Added a convolution function for CPU

---
+++ conv.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+
+// Compute the convolution of two sequences.
+int conv(int* x, int* h, int* y, int N, int M) {
+  int i, j;
+  for (i = 0; i < N + M - 1; i++) {
+    y[i] = 0;
+    for (j = 0; j < N; j++) {
+      if (i - j >= 0 && i - j < M) {
+        y[i] += x[j] * h[i - j];
+      }
+    }
+  }
+  return 0;
+}
+
+int main() {}
